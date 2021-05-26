e1a2e2d2c25b94e1196a84218b8ff000fc1098f6
Mike7373 <61974139+Mike7373@users.noreply.github.com>
2021-05-26 18:17:15 +0200

    prova

--- sito/home.php
+++ sito/home.php
@@ -8,23 +8,25 @@
     <body>
         <div id="container">    
             <div id="content">
-                <ul id="menu">
-                    <li class="menu">
-                        <a class="menu">
-                            Home
-                        </a>
-                    </li>
-                    <li class="menu">
-                        <a class="menu">
-                            News
-                        </a>
-                    </li>
-                    <li class="menu">
-                        <a class="menu">
-                            Forum
-                        </a>
-                    </li>
-                </ul>
+                <div id="nav">
+                    <ul id="menu">
+                        <li class="menu">
+                            <a class="menu">
+                                Home
+                            </a>
+                        </li>
+                        <li class="menu">
+                            <a class="menu">
+                                News
+                            </a>
+                        </li>
+                        <li class="menu">
+                            <a class="menu">
+                                Forum
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </body>
