6e95950e0bf0ebfc770d735d6adb4d163c14ec1b
Mike7373 <61974139+Mike7373@users.noreply.github.com>
2021-05-26 18:48:54 +0200

    inizio menu dinamico

--- sito/home.php
+++ sito/home.php
@@ -6,22 +6,25 @@
         </title>
     </head>
     <body>
+        <?php
+            $voce_menu = ["Home", "News",];
+        ?>
         <div id="container">    
             <div id="content">
                 <div id="nav">
                     <ul id="menu">
                         <li class="menu">
-                            <a class="menu">
+                            <a class="menu" href="">
                                 Home
                             </a>
                         </li>
                         <li class="menu">
-                            <a class="menu">
+                            <a class="menu" href="">
                                 News
                             </a>
                         </li>
                         <li class="menu">
-                            <a class="menu">
+                            <a class="menu" href="">
                                 Forum
                             </a>
                         </li>
